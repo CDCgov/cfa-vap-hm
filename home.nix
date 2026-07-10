ac584a0c9b22c7d5bf07989cc587ef628a06635b
jkislin <qxk3@cdc.gov>
2026-07-10 17:05:12 +0000

    Added r packages user library management alongside nix management for r 4.5.3

--- home.nix
+++ home.nix
@@ -95,7 +95,24 @@
       # Nix managed R and R packages
       (rWrapper.override {
         packages = with rPackages; [
+          # analytics
+          data_table
+          rpart
           tidyverse
+          # package management
+          pak
+          renv
+          # utilities
+          cowplot
+          devtools
+          ggplot2
+          jsonlite
+          languageserver
+          lintr
+          Rcpp
+          readxl
+          styler
+          yaml
         ];
       })
 
