fbde983fbb039093d8f1e5bdf4d1cf8d9de9f765
jkislin <qxk3@cdc.gov>
2026-07-14 16:39:59 +0000

    rstudiowrapper.override

--- modules/R.nix
+++ modules/R.nix
@@ -1,4 +1,27 @@
 { pkgs-unstable, ... }:
+let
+  # Shared R package set, used by both R (rWrapper) and RStudio (rstudioWrapper)
+  rPkgs = with pkgs-unstable.rPackages; [
+    # analytics
+    data_table
+    rpart
+    tidyverse
+    # package management
+    pak
+    renv
+    # utilities
+    cowplot
+    devtools
+    ggplot2
+    jsonlite
+    languageserver
+    lintr
+    Rcpp
+    readxl
+    styler
+    yaml
+  ];
+in
 {
   # home directory dotfiles
   home.file.".Rprofile".source = ../dotfiles/.Rprofile;
@@ -6,29 +29,13 @@
   # R packages (built from nixpkgs unstable to get R 4.6)
   home.packages = with pkgs-unstable; [
     # rWrapper.override gives you r itself,
-    # then the with rPackages block gives you
-    # individual packages
+    # then the packages block gives you individual packages
     (rWrapper.override {
-      packages = with rPackages; [
-        # analytics
-        data_table
-        rpart
-        tidyverse
-        # package management
-        pak
-        renv
-        # utilities
-        cowplot
-        devtools
-        ggplot2
-        jsonlite
-        languageserver
-        lintr
-        Rcpp
-        readxl
-        styler
-        yaml
-      ];
+      packages = rPkgs;
+    })
+    # rstudioWrapper gives RStudio using the same R and package set
+    (rstudioWrapper.override {
+      packages = rPkgs;
     })
   ];
 }
