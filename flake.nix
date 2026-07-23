9ddec11b76e78ac74850790e423758d1ac475397
Damon Bayer <xum8@cdc.gov>
2026-07-23 16:07:25 -0500

    add formatting to pre-commit

--- flake.nix
+++ flake.nix
@@ -9,28 +9,32 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  }; 
+  };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs =
+    { nixpkgs, home-manager, ... }:
     let
       system = builtins.currentSystem;
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations = let
-        user = builtins.getEnv "USER";
-        homedir = builtins.getEnv "HOME";
-        release = "26.05";
-      in {
-        ${user} = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ 
-            ./home.nix
-            ./modules/R.nix
-          ];
-          extraSpecialArgs = {
-            inherit user homedir release;
+    in
+    {
+      homeConfigurations =
+        let
+          user = builtins.getEnv "USER";
+          homedir = builtins.getEnv "HOME";
+          release = "26.05";
+        in
+        {
+          ${user} = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              ./home.nix
+              ./modules/R.nix
+            ];
+            extraSpecialArgs = {
+              inherit user homedir release;
+            };
           };
         };
-      };
     };
 }
