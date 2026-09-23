a9c074fa47db54c7ae521e1c8824827299e72d9b
jkislin <qxk3@cdc.gov>
2026-09-23 19:00:19 +0000

    simplify R module for quicker and lighter installs

--- modules/R.nix
+++ modules/R.nix
@@ -1,28 +1,18 @@
+# R packages defined here; this is for clarity, not technical necessity
+# i.e. we could've defined them in home.nix just the same
 { pkgs, ... }:
 let
-  # Shared package list used by both the RStudio wrapper and the
-  # command-line R wrapper so they stay in sync.
+  # Package list used by the command-line R wrapper.
   rPackageList = with pkgs.rPackages; [
-    # analytics
-    data_table
-    rpart
-    stringi
-    stringr
-    tidyverse
     # package management
     pak
     renv
     # utilities
-    cowplot
     devtools
     ggplot2
     jsonlite
     languageserver
-    lintr
     Rcpp
-    readxl
-    styler
-    yaml
   ];
 in
 {
@@ -31,11 +21,6 @@ in
 
   # R packages
   home.packages = with pkgs; [
-    # rstudioWrapper.override gives you RStudio with the packages
-    # available *inside* RStudio.
-    (rstudioWrapper.override {
-      packages = rPackageList;
-    })
     # rWrapper.override gives you a command-line `R` / `Rscript` on your
     # PATH (visible to `which R`) with the same packages available.
     (rWrapper.override {
